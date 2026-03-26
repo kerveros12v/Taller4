ef007b5dcac2eebf6d22efd4f27062f3a9766045
Leonardo-Narvaez <leonardonarvaez12@gmail.com>
2026-03-25 20:29:56 -0500

    feat: add class tienda and builder

---
+++ src/com/krakedev/tienda/tienda.java
@@ -0,0 +1,34 @@
+public class tienda {
+	private String nombreTienda;
+	private String[] productos;
+	private int totalProductos;
+
+	public String getNombreTienda() {
+		return nombreTienda;
+	}
+
+	public void setNombreTienda(String nombreTienda) {
+		this.nombreTienda = nombreTienda;
+	}
+
+	public String[] getProductos() {
+		return productos;
+	}
+
+	public void setProductos(String[] productos) {
+		this.productos = productos;
+	}
+
+	public int getTotalProductos() {
+		return totalProductos;
+	}
+
+	public void setTotalProductos(int totalProductos) {
+		this.totalProductos = totalProductos;
+	}
+    public tienda(String nombreTienda) {
+		this.nombreTienda = nombreTienda;
+		this.productos = new String[10];
+		this.totalProductos = 0;
+	}
+}
